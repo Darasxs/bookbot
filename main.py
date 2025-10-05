83a6455f910e4de4d2f7a8a11b608c8d99f0f64d
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2025-10-05 15:12:22 +0200

    project done

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_book_text(filepath):
 
 def main():
     if(len(sys.argv) != 2):
-        print("Usage: python3 main.py <path_to_book>\n")
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     print("============ *BOOKBOT* ============\n")
     book = get_book_text(sys.argv[1])
